0e32d6db69c0cdf7876edf4a610b3cbba8442ce5
hggnomm <hggnomm.dev@gmail.com>
2024-11-29 23:31:15 +0700

    update css

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
-
+import playIcon from "../assets/icons8-play-100.png";
+import pauseIcon from "../assets/icons8-pause-100.png";
 const Mainpage = ({ items }) => {
   const [selectedId, setSelectedId] = useState(1);
   const [fakeItemPosition, setFakeItemPosition] = useState({
     x: null,
     y: null,
   });
-  const [isPlaying, setIsPlaying] = useState(false); // Trạng thái phát video
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isButtonVisible, setIsButtonVisible] = useState(true);
+  const [isButtonHovered, setIsButtonHovered] = useState(false);
   const vidRef = useRef();
 
   useEffect(() => {
@@ -25,17 +29,43 @@ const Mainpage = ({ items }) => {
     const { clientX, clientY } = e; // Lấy tọa độ (x, y) của sự kiện click
     setSelectedId(id);
     setFakeItemPosition({ x: clientX, y: clientY }); // Lưu vị trí click
+
+    setIsPlaying(false); // Dừng video khi chuyển đổi
+    setIsButtonVisible(true); // Hiển thị nút khi chuyển đổi video
+    setIsButtonHovered(false); // Đặt trạng thái hover nút về mặc định
   };
 
   const handlePlayPause = () => {
-    debugger
     const video = vidRef.current;
     if (isPlaying) {
       video.pause(); // Dừng video
     } else {
       video.play(); // Phát video
+      setIsButtonVisible(false); // Ẩn nút khi video bắt đầu phát
     }
     setIsPlaying(!isPlaying); // Cập nhật trạng thái phát video
+  };
+
+  const handleMouseEnter = () => {
+    setIsButtonHovered(true); // Khi hover vào nút, giữ nút hiển thị
+  };
+
+  const handleMouseLeave = () => {
+    setIsButtonHovered(false); // Khi rời chuột khỏi nút, kiểm tra nếu không hover vào video thì ẩn nút
+    if (!isPlaying) {
+      setIsButtonVisible(false); // Ẩn nút nếu video không chơi và không hover vào nút
+    }
+  };
+
+  const handleButtonMouseEnter = () => {
+    setIsButtonHovered(true); // Khi hover vào nút, giữ nút hiển thị
+  };
+
+  const handleButtonMouseLeave = () => {
+    setIsButtonHovered(false); // Khi rời chuột khỏi nút, kiểm tra nếu không hover vào video thì ẩn nút
+    if (!isPlaying) {
+      setIsButtonVisible(false); // Giữ nút ẩn khi video không phát
+    }
   };
 
   const renderMedia = (item, isFakeItem = false) => {
@@ -47,8 +77,10 @@ const Mainpage = ({ items }) => {
           alt={`Item ${item.id}`}
           className={`${
             isFakeItem ? "h-[65%] border-2 border-[#1e2721]" : "w-full h-full"
-          } rounded-xl shadow-[0_20px_120px_rgba(26,_26,_29,_0.7)] `}
+          } rounded-xl shadow-[0_20px_100px_rgba(238,_238,_238,_0.9)] `}
           disablePictureInPicture
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         />
       );
     } else {
@@ -63,17 +95,17 @@ const Mainpage = ({ items }) => {
   };
 
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen min-w-full bg-[#D9D9D9] overflow-hidden ">
+    <div className="relative flex flex-col items-center justify-center min-h-screen min-w-full bg-[#9f9f9f] overflow-hidden">
       {/* Fake item */}
       <div className="w-full h-full">
         {fakeItemPosition.x !== null && fakeItemPosition.y !== null && (
           <motion.div
-            className="absolute rounded-2xl overflow-hidden w-full flex justify-center items-center "
+            className="absolute rounded-2xl overflow-hidden w-full flex justify-center items-center"
             style={{
               left: fakeItemPosition.x - 32,
               top: fakeItemPosition.y - 32,
             }}
-            key={`fake-item-${selectedId}`} // Thêm key mới khi selectedId thay đổi
+            key={`fake-item-${selectedId}`}
             initial={{ opacity: 0.2, width: 64, height: 64 }}
             animate={{
               left: "50%",
@@ -81,7 +113,7 @@ const Mainpage = ({ items }) => {
               x: "-50%",
               y: "-50%",
               width: "100%",
-              height: "100%",
+              height: "100vh",
               opacity: 1,
             }}
             transition={{
@@ -92,11 +124,16 @@ const Mainpage = ({ items }) => {
             }}
           >
             <button
-              className="absolute text-white bg-black px-4 py-2 rounded-lg z-20"
-              onClick={handlePlayPause} // Khi nhấn nút, gọi handlePlayPause
+              className={`absolute text-white bg-black  p-3 rounded-full z-20 transition-opacity duration-500 ${
+                isButtonVisible || isButtonHovered ? "opacity-50" : "opacity-0"
+              }`}
+              onClick={handlePlayPause}
+              onMouseEnter={handleButtonMouseEnter}
+              onMouseLeave={handleButtonMouseLeave}
             >
-              {isPlaying ? "PAUSE" : "PLAY"}
+              <img width={36} src={isPlaying ? pauseIcon : playIcon} />
             </button>
+
             {renderMedia(
               items.find((item) => item.id === selectedId),
               true
