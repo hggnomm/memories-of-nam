7215060772e191d0a7b954879b40b0a44f81f45b
hggnomm <hggnomm.dev@gmail.com>
2024-12-02 09:49:50 +0700

    fix code mobile

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div className="fixed xl:top-10 xl:left-11 top-5 left-5 z-20 w-screen">
       <img src={logoNom} alt="" className="w-32 mb-6" />
-      <div className="flex xl:block justify-between items-center mr-6">
+      <div className="flex xl:block justify-between items-center mr-10">
         <div>
           <span className="text-white ml-1 mb-1 block">
             Began: 1-12-2024
@@ -14,7 +14,7 @@ const Logo = () => {
         </div>
 
         <div>
-          <span className="text-gray-300 ml-1 xl:mt-8 block w-44 italic text-base text-[0.8rem]">
+          <span className="text-gray-300 ml-1 xl:mt-8 block w-44 italic text-sm xl:text-base">
             Mọi khoảnh khắc và sự hoài niệm, nó chính là yếu tố tạo nên giá trị cho cuộc
             sống.
           </span>
